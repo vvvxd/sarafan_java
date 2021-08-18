5f00889ab89e0e03d901a6e94d497bb8d07dd29b
Valentin Vlasov <40793527+vvvxd@users.noreply.github.com>
2021-08-18 22:07:19 +0300

    First controller

---
+++ src/main/java/com/example/sarafan_java/SarafanJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarafan_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarafanJavaApplication.class, args);
+    }
+
+}
